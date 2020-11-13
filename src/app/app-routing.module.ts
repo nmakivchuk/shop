5eaedc88b54a2d3abee72daacf905efa5b0d92dd
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-11-14 00:50:46 +0200

    solution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [{
-  path: 'shoppingcart',
-  loadChildren: 'app/shopping-cart/shopping-cart.module#ShoppingCartModule'
+  path: 'cart',
+  // loadChildren: 'app/shopping-cart/shopping-cart.module#ShoppingCartModule'
+  component: CartComponent
 }];
 
 @NgModule({
