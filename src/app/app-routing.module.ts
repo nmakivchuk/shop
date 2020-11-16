9b814bec80b130e377f2db85082acbcb85939d28
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-11-16 22:34:16 +0200

    Routes and cart list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{
-  path: 'cart',
-  // loadChildren: 'app/shopping-cart/shopping-cart.module#ShoppingCartModule'
-  component: CartComponent
-}];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GadgetService} from '../gadget.service';
+import { NgRedux } from '@angular-redux/store';
+import { InitialState } from '../store/reducer';
+import { Product } from '../product/product.component';
 
 @Component({
   selector: 'app-cart',
@@ -9,9 +12,13 @@ import { GadgetService} from '../gadget.service';
 export class CartComponent implements OnInit {
   cartProducts: any;
 
-  constructor() {}
-
-  ngOnInit(): void {
-
+  constructor(private ngRedux: NgRedux<InitialState>) {
+    this.ngRedux
+      .select<Array<Product>>('cart')
+      .subscribe((items: Array<Product>) => {
+        this.cart = items;
+      });
   }
+  cart: Array<Product>;
+  ngOnInit() {}
 }
